9608d1415cd90d23332a9023ebc650cffeb29711
Sagar Raj Thapaliya <sr3959596@gmail.com>
2022-08-13 15:34:13 +0545

    edited css of main page

--- tumordetection_frontend/src/App.js
+++ tumordetection_frontend/src/App.js
@@ -1,58 +1,66 @@
-
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
-import './App.css';
-import { Header, MainWrapper } from './header';
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import "./App.css";
+import { Header, MainWrapper } from "./header";
 
 function App() {
-  return (
+	return (
+		<MainWrapper>
+			<div className="test">
+				<Header className="header">Tumor Detection Application </Header>
+			</div>
+			<div className="mid-bar"></div>
+			<div className="area">
+				<div className="button-container">
+					<Button
+						classname="but"
+						style={{
+							maxWidth: "8rem",
+							maxHeight: "2rem",
+							minWidth: "10rem",
+							minHeight: "3rem",
+						}}
+						variant="contained"
+						component="label"
+					>
+						Upload
+						<input hidden accept="image/*" multiple type="file" />
+					</Button>
 
-    <MainWrapper>
-      <div className='test'>
-        <Header className='header'>TUMO</Header>
-      </div>
-      <div className='mid-bar'></div>
-      <div className='area'>
-      <div className='button-container'>
-        <Button classname="but" style={{
-          maxWidth: "8rem",
-          maxHeight: "2rem",
-          minWidth: "10rem",
-          minHeight: "3rem"
-        }} variant="contained" component="label">
-          Upload
-          <input hidden accept="image/*" multiple type="file" />
-        </Button>
+					<IconButton
+						color="primary"
+						aria-label="upload picture"
+						component="label"
+					>
+						<input hidden accept="image/*" type="file" />
+						<PhotoCameraIcon
+							style={{
+								zIndex: 111,
+								maxWidth: "1rem",
+								maxHeight: "2rem",
+								minWidth: "6rem",
+								minHeight: "3rem",
+							}}
+						/>
+					</IconButton>
+				</div>
 
-        <IconButton color="primary" aria-label="upload picture" component="label">
-          <input hidden accept="image/*" type="file" />
-          <PhotoCameraIcon style={{
-            zIndex: 111,
-            maxWidth: "1rem",
-            maxHeight: "2rem",
-            minWidth: "6rem",
-            minHeight: "3rem"
-          }} />
-        </IconButton>
-      </div>
-
-
-        <ul class="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </MainWrapper>
-  );
+				<ul class="circles">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		</MainWrapper>
+	);
 }
 
 export default App;
